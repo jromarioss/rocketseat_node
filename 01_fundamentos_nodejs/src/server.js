532c1d1e1bd9d08fc4ceb4d25ed5ff57dd2f4d4b
JromarioSs <jromario2014@gmail.com>
2025-01-05 12:36:05 -0300

    03 rotas HTTP

--- 01_fundamentos_nodejs/src/server.js
+++ 01_fundamentos_nodejs/src/server.js
@@ -1,7 +1,17 @@
 import http from 'node:http';
 
-const server = http.createServer((request, response) => {
-  return response.end("hello world again!");
+const server = http.createServer((req, res) => {
+  const { method, url } = req;
+
+  if (method === 'GET' && url === '/users') {
+    return res.end("listagem de usuários")
+  }
+
+  if (method === 'POST' && url === '/users') {
+    return res.end("criação de usuários")
+  }
+
+  return res.end("hello world again!");
 });
 
 server.listen(3333);
