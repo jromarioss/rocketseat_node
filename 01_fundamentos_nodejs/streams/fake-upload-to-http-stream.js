aafbdca01ca94434bbe2801fdc41be48a7cb5eda
JromarioSs <jromario2014@gmail.com>
2025-01-05 19:54:34 -0300

    09 Consumindo uma stream completa

--- 01_fundamentos_nodejs/streams/fake-upload-to-http-stream.js
+++ 01_fundamentos_nodejs/streams/fake-upload-to-http-stream.js
@@ -21,4 +21,8 @@ class OneToHundredStream extends Readable {
 fetch('http://localhost:3334', {
   method: 'POST',
   body: new OneToHundredStream(),
+}).then(response => {
+  return response.text();
+}).then(data => {
+  console.log(data);
 });
