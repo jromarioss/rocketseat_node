354db23dc7112cac2a975a737fba48253f3b5b97
JromarioSs <jromario2014@gmail.com>
2025-01-06 18:07:40 -0300

    16 Validando existência do cookie

--- 2023/02_api_rest_nodejs/src/routes/transaction.ts
+++ 2023/02_api_rest_nodejs/src/routes/transaction.ts
@@ -2,21 +2,33 @@ import { knex } from '../database';
 import { z } from 'zod';
 import { FastifyInstance } from 'fastify';
 import { randomUUID } from 'node:crypto';
+import { checkSessionIdExists } from '../middleware/check-session-id-exists';
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/', async () => {
-    const transactions = await knex('transactions').select();
+  app.get('/', {
+    preHandler: [checkSessionIdExists]
+  }, async (request, reply) => {
+    const { sessionId } = request.cookies;
+
+    const transactions = await knex('transactions')
+      .where('session_id', sessionId)
+      .select();
     return { transactions };
   });
 
-  app.get('/:id', async (request) => {
+  app.get('/:id', {
+    preHandler: [checkSessionIdExists]
+  },async (request) => {
     const getTransactionParamsSchema = z.object({
       id: z.string().uuid()
     });
 
     const { id } = getTransactionParamsSchema.parse(request.params);
+    const { sessionId } = request.cookies;
 
-    const transaction = await knex('transactions').where('id', id).first();
+    const transaction = await knex('transactions')
+      .where({ id, session_id: sessionId })
+      .first();
     return { transaction };
   });
 
