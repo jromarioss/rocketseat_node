5690edd8741571cf27c90430dc1956f7cc9bb70e
JromarioSs <jromario2014@gmail.com>
2025-01-05 21:40:49 -0300

    14 Persistindo banco de dados

--- 01_fundamentos_nodejs/src/database.js
+++ 01_fundamentos_nodejs/src/database.js
@@ -1,18 +1,34 @@
+import fs from 'node:fs/promises';
+
+const databasePath = new URL('../database/db.json', import.meta.url);
+
 export class Database {
   #database = {};
 
+  constructor() {
+    fs.readFile(databasePath, 'utf-8')
+      .then(data => { this.#database = JSON.parse(data) })
+      .catch(() => { this.#persist() })
+  }
+
+  #persist() {
+    fs.writeFile(databasePath, JSON.stringify(this.#database));
+  }
+
+  select(table) {
+    const data = this.#database[table] ?? [];
+    return data;
+  }
+
   insert(table, data) {
-    if (Array.isArray(this.database[table])) {
+    if (Array.isArray(this.#database[table])) {
       this.#database[table].push(data);
     } else {
       this.#database[table] = [data];
     }
 
-    return data;
-  }
+    this.#persist();
 
-  select(table) {
-    const data = this.#database[table] ?? [];
     return data;
   }
 }
