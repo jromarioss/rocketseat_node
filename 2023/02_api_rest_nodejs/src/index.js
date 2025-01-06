2ecb282864ef4dca20fa8e5e7e63859cf1ae85df
JromarioSs <jromario2014@gmail.com>
2025-01-06 11:51:08 -0300

    02 Criando a aplicação

--- 2023/02_api_rest_nodejs/src/index.js
+++
@@ -1,4 +0,0 @@
-function calculateYearOfUser(user) {
-    return new Date().getFullYear() - user.birthYear;
-}
-console.log(calculateYearOfUser({ birthYear: 1998 }));
--- 2023/02_api_rest_nodejs/src/index.ts
+++
@@ -1,9 +0,0 @@
-interface IUser {
-  birthYear: number;
-}
-
-function calculateYearOfUser(user: IUser) {
-  return new Date().getFullYear() - user.birthYear;
-}
-
-console.log(calculateYearOfUser({ birthYear: 1998 }));
