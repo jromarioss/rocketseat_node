d3725ad07771ffac3eff2d7443ccbe604bf889fc
JromarioSs <jromario2014@gmail.com>
2025-01-06 16:11:50 -0300

    06 Realizando queries com o knex

--- 2023/02_api_rest_nodejs/src/server.ts
+++ 2023/02_api_rest_nodejs/src/server.ts
@@ -1,11 +1,20 @@
 import fastify from 'fastify';
 import { knex } from './database';
+import crypto from 'node:crypto';
 
 const app = fastify();
 
 app.get('/hello', async () => {
-  const table = await knex('sqlite_schema').select('*');
-  return table;
+  // const transation = await knex('transactions').insert({
+  //   id: crypto.randomUUID(),
+  //   title: 'Transação de teste',
+  //   amount: 1000
+  // }).returning('*');
+
+  //const transation = await knex('transactions').select('*');
+  const transation = await knex('transactions').where('amount', 1000).select('*');
+
+  return transation;
 });
 
 app.listen({
