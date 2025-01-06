9e2adf816d31b92d9580b912d15e33eb18dd04c8
JromarioSs <jromario2014@gmail.com>
2025-01-06 09:41:42 -0300

    15 Criando ID único com universal UUID

--- 01_fundamentos_nodejs/src/server.js
+++ 01_fundamentos_nodejs/src/server.js
@@ -1,6 +1,7 @@
 import http from 'node:http';
 import { json } from './middlewares/json.js';
 import { Database } from './database.js';
+import { randomUUID } from "node:crypto";
 
 const database = new Database;
 
@@ -12,16 +13,16 @@ const server = http.createServer(async (req, res) => {
   if (method === 'GET' && url === '/users') {
     const users = database.select('users');
 
-    return res.end(JSON.stringify(users))
+    return res.end(JSON.stringify(users));
   }
 
   if (method === 'POST' && url === '/users') {
     const { name, email } = req.body;
 
-    const user = { name, email };
+    const user = { id: randomUUID(), name, email };
     database.insert('users', user);
 
-    return res.writeHead(201).end()
+    return res.writeHead(201).end();
   }
 
   return res.writeHead(404).end();
