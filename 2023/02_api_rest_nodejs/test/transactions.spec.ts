69f33cae6bcf834248afbd7b583e413a9ed1a8dc
JromarioSs <jromario2014@gmail.com>
2025-01-06 21:29:30 -0300

    20 Categorizando os testes

--- 2023/02_api_rest_nodejs/test/transactions.spec.ts
+++ 2023/02_api_rest_nodejs/test/transactions.spec.ts
@@ -1,23 +1,25 @@
-import { expect, test, beforeAll, afterAll } from 'vitest';
+import { expect, it, beforeAll, afterAll, describe } from 'vitest';
 import request from 'supertest';
 import { app } from '../src/app';
 
-beforeAll(async () => {
-  await app.ready();
-});
-
-afterAll(async () => {
-  await app.close();
-});
-
-test('O usuário consegue criar uma nova transação', async () => {
-  const response = await request(app.server)
-    .post('/transactions')
-    .send({
-      title: 'New transaction',
-      amount: 1000,
-      type: 'credit'
-    });
-
-  expect(response.statusCode).toEqual(201);
+describe('Transactions routes', () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+  
+  afterAll(async () => {
+    await app.close();
+  });
+  
+  it('should be able to create a new transaction', async () => {
+    const response = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 1000,
+        type: 'credit'
+      });
+  
+    expect(response.statusCode).toEqual(201);
+  });
 });
