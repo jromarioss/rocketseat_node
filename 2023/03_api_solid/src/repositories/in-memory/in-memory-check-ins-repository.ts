f81c91cadb0d7f06464bad9f680be3f91ecc9d77
JromarioSs <jromario2014@gmail.com>
2025-01-12 14:26:59 -0300

    30 TDD & Mocking

--- 2023/03_api_solid/src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ 2023/03_api_solid/src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -18,4 +18,10 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
 
     return checkIn;
   }
+
+  async findByUserIdOnDate(userId: string, date: Date) {
+    const checkOnSameDate = this.items.find(checkIn => checkIn.user_id === userId);
+    if(!checkOnSameDate) return null;
+    return checkOnSameDate;
+  }
 }
--- 2023/03_api_solid/src/use-cases/checkin.ts
+++ 2023/03_api_solid/src/use-cases/checkin.ts
@@ -15,6 +15,12 @@ export class CheckinUseCase {
   constructor(private readonly checkInsRepository: CheckInsRepository) {}
 
   async execute({ userId, gymId }: CheckinUseCaseRequest): Promise<CheckinUseCaseResponse> {
+    const checkInOnSameDate = await this.checkInsRepository.findByUserIdOnDate(userId, new Date());
+
+    if (checkInOnSameDate) {
+      throw new Error();
+    }
+
     const checkIn = await this.checkInsRepository.create({
       gym_id: gymId,
       user_id: userId
