3c068fe275c92f25f66523afba21bbf6dc9855d5
JromarioSs <jromario2014@gmail.com>
2025-01-14 22:03:50 -0300

    35 Caso de uso de metricas

--- 2023/03_api_solid/src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ 2023/03_api_solid/src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -38,4 +38,8 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
   async findManyByUserId(userId: string, page: number) {
     return this.items.filter(item => item.user_id === userId).slice((page - 1) * 20, page * 20);
   }
+
+  async countByUserId(userId: string) {
+    return this.items.filter(item => item.user_id === userId).length;
+  }
 }
