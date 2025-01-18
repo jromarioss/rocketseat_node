371d857690b69cd4b8292232ddc06fadfe4de20f
JromarioSs <jromario2014@gmail.com>
2025-01-18 08:58:48 -0300

    39 Validando horario do check-in

--- 2023/03_api_solid/src/use-cases/validate-check-in.spec.ts
+++ 2023/03_api_solid/src/use-cases/validate-check-in.spec.ts
@@ -3,6 +3,7 @@ import { ValidateCheckInUseCase } from "./validate-check-in.ts";
 import { InMemoryCheckInsRepository } from "@/repositories/in-memory/in-memory-check-ins-repository";
 import { afterEach } from "node:test";
 import { ResourceNotFoundError } from "./errors/resource-not-found-error.js";
+import { LateCheckInValidateError } from "./errors/late-check-in-validate-error.js";
 
 let checkInsRepository: InMemoryCheckInsRepository;
 let sut: ValidateCheckInUseCase;
@@ -12,11 +13,11 @@ describe("Validate check-in UseCase", () => {
     checkInsRepository = new InMemoryCheckInsRepository();
     sut = new ValidateCheckInUseCase(checkInsRepository);
 
-    //vi.useFakeTimers();
+    vi.useFakeTimers();
   });
 
   afterEach(() => {
-    //vi.useRealTimers();
+    vi.useRealTimers();
   });
 
   it("should be able to validate check-in", async () => {
@@ -32,8 +33,26 @@ describe("Validate check-in UseCase", () => {
   });
 
   it("should not be able to validate an inexsistent check-in", async () => {
-    expect(() =>
+    await expect(() =>
       sut.execute({ checkInId: 'id_qualquer' })
     ).rejects.toBeInstanceOf(ResourceNotFoundError);
+  });
+
+  it("should not be able to validate the check-in after 20 minutes of its creation", async () => {
+    vi.setSystemTime(new Date(2023, 0, 1, 13, 40));
+    
+    const createdCheckIn = await checkInsRepository.create({
+      gym_id: 'gym-01',
+      user_id: 'user-01'
+    });
+
+    const twentyOneMinutesInMS = 1000 * 60 * 21;
+
+    vi.advanceTimersByTime(twentyOneMinutesInMS);
+
+    
+    await expect(() => 
+      sut.execute({ checkInId: createdCheckIn.id })
+    ).rejects.toBeInstanceOf(LateCheckInValidateError);
   });
 });
