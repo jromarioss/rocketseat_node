d6ce8223eb8ac4dfc15f5aac02888981e14e418c
JromarioSs <jromario2014@gmail.com>
2025-02-25 20:24:07 -0300

    48 Test environment do prisma

--- 2023/03_api_solid/prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ 2023/03_api_solid/prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,41 +1,45 @@
-import 'dotenv/config'
-import { Environment } from 'vitest'
-import { randomUUID } from 'node:crypto'
-import { PrismaClient } from '@prisma/client'
-import { execSync } from 'node:child_process'
+import "dotenv/config";
+import { Environment } from "vitest";
+import { randomUUID } from "node:crypto";
+import { PrismaClient } from "@prisma/client";
+import { execSync } from "node:child_process";
 
 //postgresql://docker:docker@localhost:5432/apisolid?schema=public
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 function generateDatabaseURL(schema: string) {
   if (!process.env.DATABASE_URL) {
-    throw new Error('Please provide a DATABASE_URL environment variable.')
+    throw new Error("Please provide a DATABASE_URL environment variable.");
   }
 
-  const url = new URL(process.env.DATABASE_URL)
+  const url = new URL(process.env.DATABASE_URL);
+  url.searchParams.set("schema", schema);
 
-  url.searchParams.set('schema', schema) //substitui o schema por chema que eu estou mandando
-
-  return url.toString()
+  return url.toString();
 }
 
-export default <Environment> {
-  name: 'prisma',
-  async setup() { //qual código quero executar antes do meus testes
-    const schema = randomUUID()
-    const databaseURL = generateDatabaseURL(schema)
+const prismaEnvironment: Environment = {
+  name: "prisma",
+  transformMode: "ssr",
 
-    process.env.DATABASE_URL = databaseURL //sobreescreve as variável por a nova com schema
+  async setup() {
+    const schema = randomUUID();
+    const databaseURL = generateDatabaseURL(schema);
 
-    execSync('npx prisma migrate deploy') //como se fosse um comando executando no terminal
+    process.env.DATABASE_URL = databaseURL;
+
+    execSync("npx prisma migrate deploy");
+    
     return { 
       async teardown() {
         await prisma.$executeRawUnsafe(
-          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
-        )
-        await prisma.$disconnect()
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`
+        );
+        await prisma.$disconnect();
       }
-    } //um metódo que eu quero executar após meus metódos executarem
+    };
   },
-}
+};
+
+export default prismaEnvironment;
