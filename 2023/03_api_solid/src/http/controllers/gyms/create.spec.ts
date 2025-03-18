69bdd5ae7f767ffd957081fcc5e33eb564a04b7a
JromarioSs <jromario2014@gmail.com>
2025-03-17 21:27:43 -0300

    55 Teste E2E de rotas de academias

---
+++ 2023/03_api_solid/src/http/controllers/gyms/create.spec.ts
@@ -0,0 +1,31 @@
+import request from "supertest";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import { app } from "@/app";
+import { createAndAuthenticateUser } from "@/utils/test/create-and-authenticate-user";
+
+describe("Create Gyms (e2e)", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should be able to create a gym", async () => {
+    const { token } = await createAndAuthenticateUser(app, true);
+
+    const response = await request(app.server)
+      .post("/gyms")
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        title: "JavaScript Gym",
+        description: "Some description",
+        phone: "15998658958",
+        latitude: -23.1705951,
+        longitude: -47.7639674,
+      });
+
+    expect(response.statusCode).toEqual(201);
+  });
+})
