9efe3c9a64d4cc150cd6f97d14e0ea44115bf572
JromarioSs <jromario2014@gmail.com>
2025-03-20 20:53:50 -0300

    57 Estratégia de refresh de token

--- 2023/03_api_solid/src/app.ts
+++ 2023/03_api_solid/src/app.ts
@@ -4,14 +4,23 @@ import { gymsRoutes } from "./http/controllers/gyms/routes";
 import { checkIns } from "./http/controllers/check-ins/routes";
 import { ZodError } from "zod";
 import { env } from "./env";
+import fastifyCookie from "@fastify/cookie";
 import fastifyJwt from "@fastify/jwt";
 
 export const app = fastify();
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false
+  },
+  sign: {
+    expiresIn: '10m'
+  }
 });
 
+app.register(fastifyCookie);
 app.register(usersRoutes);
 app.register(gymsRoutes);
 app.register(checkIns);
@@ -24,11 +33,8 @@ app.setErrorHandler((error, request, reply) => {
     });
   }
 
-  if (env.NODE_ENV !== "production") {
-    console.error(error);
-  } else {
-    // TODO: fazer log depois
-  }
+  if (env.NODE_ENV !== "production") console.error(error);
+  else // TODO: fazer log depois
 
   return reply.status(500).send({
     message: "Internal server error."
