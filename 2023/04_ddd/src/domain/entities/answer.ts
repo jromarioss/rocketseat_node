34a07e719777276655bc5880eb1271fd283beba5
JromarioSs <jromario2014@gmail.com>
2025-03-26 21:30:16 -0300

    04 – Mapeando relacionamentos

--- 2023/04_ddd/src/domain/entities/answer.ts
+++ 2023/04_ddd/src/domain/entities/answer.ts
@@ -1,11 +1,21 @@
 import { randomUUID } from "node:crypto";
 
+interface AnswerProps {
+  content: string;
+  authorId: string;
+  questionid: string;
+}
+
 export class Answer {
   public id: string;
   public content: string;
+  public authorId: string;
+  public questionid: string;
 
-  constructor(content: string, id?: string) {
-    this.content = content;
+  constructor(props: AnswerProps, id?: string) {
+    this.content = props.content;
+    this.authorId = props.authorId;
+    this.questionid = props.questionid;
     this.id = id ?? randomUUID();
   }
 }
