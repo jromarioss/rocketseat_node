1a54ffd0f46407d6e0c37cf0acfb7f173a319876
JromarioSs <jromario2014@gmail.com>
2025-03-31 20:28:59 -0300

    11 – Getters & setters das entidades

--- 2023/04_ddd/src/domain/entities/answer.ts
+++ 2023/04_ddd/src/domain/entities/answer.ts
@@ -12,8 +12,37 @@ interface AnswerProps {
 }
 
 export class Answer extends Entity<AnswerProps> {
+  get authorId() {
+    return this.props.authorId;
+  }
+
+  get questionid() {
+    return this.props.questionid;
+  }
+
   get content() {
     return this.props.content;
+  }
+
+  get createdAt() {
+    return this.props.createdAt;
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt;
+  }
+
+  get exerpt() {
+    return this.content.substring(0, 120).trimEnd().concat("...");
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+
+  set content(content: string) {
+    this.props.content = content;
+    this.touch();
   }
 
   static create(props: Optional<AnswerProps, "createdAt">, id?: UniqueEntityId) {
