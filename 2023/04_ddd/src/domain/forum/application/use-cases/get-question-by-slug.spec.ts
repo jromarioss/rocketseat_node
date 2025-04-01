f08ba7526aba8e2c4299e8f5eed43a64fba32e49
JromarioSs <jromario2014@gmail.com>
2025-04-01 20:56:08 -0300

    18 – Caso de uso buscar pergunta pelo slug

---
+++ 2023/04_ddd/src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
@@ -0,0 +1,34 @@
+import { expect } from "vitest";
+import { InMemoryQuestionsRepository } from "test/repositories/in-memory-questions-repository";
+import { GetQuestionBySlugUseCase } from "./get-question-by-slug";
+import { Question } from "../../enterprise/entities/question";
+import { Slug } from "../../enterprise/entities/value-objects/slug";
+import { UniqueEntityId } from "@/core/entities/unique-entity-id";
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
+let sut: GetQuestionBySlugUseCase;
+
+describe("Get question by slug", () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository();
+    sut = new GetQuestionBySlugUseCase(inMemoryQuestionsRepository);
+  });
+
+  it("should be able to get a question by slug", async () => { 
+    const newQuestion = Question.create({
+      authorId: new UniqueEntityId(),
+      slug: Slug.createFromText("example-question"),
+      title: "Example question",
+      content: "Example content"
+    });
+
+    inMemoryQuestionsRepository.create(newQuestion);
+
+    const { question } = await sut.execute({
+      slug: "example-question"
+    });
+  
+    expect(question.id).toBeTruthy();
+    expect(question.title).toEqual(newQuestion.title);
+  });
+});
