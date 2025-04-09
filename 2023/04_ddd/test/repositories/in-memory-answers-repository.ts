712be828a55c6f270817f327d34294aa7ef59fca
JromarioSs <jromario2014@gmail.com>
2025-04-08 21:14:27 -0300

    27 – Caso de uso listar respostas da pergunta

--- 2023/04_ddd/test/repositories/in-memory-answers-repository.ts
+++ 2023/04_ddd/test/repositories/in-memory-answers-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from "@/core/repositories/pagination-params";
 import { AnswersRepository } from "@/domain/forum/application/repositories/answers-repository";
 import { Answer } from "@/domain/forum/enterprise/entities/answer";
 
@@ -21,8 +22,16 @@ export class InMemoryAnswersRepository implements AnswersRepository {
     this.items.splice(itemIndex, 1);
   }
 
-    async save(answer: Answer) {
-      const itemIndex = this.items.findIndex(item => item.id === answer.id);
-      this.items[itemIndex] = answer;
-    }
+  async save(answer: Answer) {
+    const itemIndex = this.items.findIndex(item => item.id === answer.id);
+    this.items[itemIndex] = answer;
+  }
+
+  async findManyByQuestionId(questionId: string, { page }: PaginationParams) {
+    const answers = this.items
+      .filter((item) => item.questionId.toString() === questionId)
+      .slice((page - 1) * 20, page * 20)
+
+    return answers;
+  }
 }
