0967d35ffa52134dae1577070bbacab1e74b1ce1
JromarioSs <jromario2014@gmail.com>
2025-04-12 11:07:47 -0300

    41 – Class base be AggregateRoot

--- 2023/04_ddd/src/domain/forum/enterprise/entities/question.ts
+++ 2023/04_ddd/src/domain/forum/enterprise/entities/question.ts
@@ -1,8 +1,8 @@
 import dayjs from "dayjs";
-import { Entity } from "@/core/entities/entity";
 import { UniqueEntityId } from "@/core/entities/unique-entity-id";
 import { Optional } from "@/core/types/optional";
 import { Slug } from "./value-objects/slug";
+import { AggregateRoot } from "@/core/entities/aggregate-root";
 
 export interface QuestionProps {
   authorId: UniqueEntityId;
@@ -15,7 +15,7 @@ export interface QuestionProps {
   updatedAt?: Date;
 }
 
-export class Question extends Entity<QuestionProps> {
+export class Question extends AggregateRoot<QuestionProps> {
   get authorId() {
     return this.props.authorId;
   }
