b747ba304e0d093b19a2101249c59dfcdcbcf33e
JromarioSs <jromario2014@gmail.com>
2025-04-14 21:01:54 -0300

    06 – Criando serviço do prisma

--- 2023/05_nestjs/src/app.controller.ts
+++ 2023/05_nestjs/src/app.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get } from "@nestjs/common";
 import { AppService } from "./app.service";
+import { PrismaService } from "./prisma/prisma.service";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly prisma: PrismaService  
+  ) {}
 
   @Get("/hello")
-  getHello(): string {
+  async getHello() {
     return this.appService.getHello();
   }
 }
