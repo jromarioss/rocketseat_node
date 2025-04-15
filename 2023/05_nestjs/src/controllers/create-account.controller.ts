cb32bf35498fc643d3ffa5f5e4fa89fbf88f143a
JromarioSs <jromario2014@gmail.com>
2025-04-14 21:28:11 -0300

    08 -  Gerando hash da senha

--- 2023/05_nestjs/src/controllers/create-account.controller.ts
+++ 2023/05_nestjs/src/controllers/create-account.controller.ts
@@ -1,6 +1,7 @@
 import { ConflictException } from "@nestjs/common";
 import { Body, Controller, HttpCode, Post } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
+import { hash } from "bcryptjs";
 
 @Controller("/accounts")
 export class CreateAccountController {
@@ -19,9 +20,13 @@ export class CreateAccountController {
       throw new ConflictException("User with same e-mail address already exists.");
     }
 
+    const hashedPassword = await hash(password, 8);
+
     await this.prisma.user.create({
       data: {
-        name, email, password
+        name,
+        email,
+        password: hashedPassword
       }
     });
   }
