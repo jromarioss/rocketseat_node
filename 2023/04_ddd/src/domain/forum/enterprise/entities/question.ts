e3b9b5eb9b39aa900be2200f1d1d1310a475b5f8
JromarioSs <jromario2014@gmail.com>
2025-04-29 21:38:52 -0300

    56 – Fluxo de melhor resposta

--- 2023/04_ddd/src/domain/forum/enterprise/entities/question.ts
+++ 2023/04_ddd/src/domain/forum/enterprise/entities/question.ts
@@ -4,6 +4,7 @@ import { Optional } from "@/core/types/optional";
 import { Slug } from "./value-objects/slug";
 import { AggregateRoot } from "@/core/entities/aggregate-root";
 import { QuestionAttachmentList } from "./question-attachment-list";
+import { QuestionBestAnswerChosenEvent } from "../events/question-best-answer-chosen-event";
 
 export interface QuestionProps {
   authorId: UniqueEntityId;
@@ -78,7 +79,14 @@ export class Question extends AggregateRoot<QuestionProps> {
   }
 
   set bestAnswerId(bestAnswerId: UniqueEntityId | undefined) {
+    if (bestAnswerId === undefined) return;
+
+    if (this.props.bestAnswerId === undefined || !bestAnswerId.equals(this.props.bestAnswerId)) {
+      this.addDomainEvent(new QuestionBestAnswerChosenEvent(this, bestAnswerId));
+    }
+
     this.props.bestAnswerId = bestAnswerId;
+
     this.touch();
   }
 
--- 2023/04_ddd/test/repositories/in-memory-questions-repository.ts
+++ 2023/04_ddd/test/repositories/in-memory-questions-repository.ts
@@ -1,3 +1,4 @@
+import { DomainEvents } from "@/core/events/domain-events";
 import { PaginationParams } from "@/core/repositories/pagination-params";
 import { QuestionAttachmentsRepository } from "@/domain/forum/application/repositories/question-attachments-repository";
 import { QuestionsRepository } from "@/domain/forum/application/repositories/questions-repository";
@@ -9,7 +10,9 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
   constructor(private questionAttachmentsRepository: QuestionAttachmentsRepository) {}
 
   async create(question: Question) {
-    this.items.push(question)
+    this.items.push(question);
+
+    DomainEvents.dispatchEventsForAggregate(question.id);
   }
 
   async findBySlug(slug: string) {
@@ -37,6 +40,8 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
   async save(question: Question) {
     const itemIndex = this.items.findIndex(item => item.id === question.id);
     this.items[itemIndex] = question;
+
+    DomainEvents.dispatchEventsForAggregate(question.id);
   }
 
   async findManyRecent({ page }: PaginationParams) {
